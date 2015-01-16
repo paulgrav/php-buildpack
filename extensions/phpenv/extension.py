73a374429c60a4beac7d019cdc6af4ba4ffd95fe
Paul Grave <paul.grave@bskyb.com>
2015-01-16 16:02:18 +0000

    added: extension that allows for environment specific php.ini files

---
+++ extensions/phpenv/extension.py
@@ -0,0 +1,25 @@
+from extension_helpers import PHPExtensionHelper
+import os
+
+_log = logging.getLogger('PHPConfigChooser')
+
+
+class PHPConfigChooser(PHPExtensionHelper):
+    def __init__(self, ctx):
+        PHPExtensionHelper.__init__(self, ctx)
+
+    def _configure(self):
+
+        if not os.environ.get('ENV'):
+            return
+
+        env = os.environ.get('ENV')
+        php_ini_path = os.path.join(self._ctx['BUILD_DIR'],
+                                         'php', 'etc', 'php.ini')
+        new_ini_path = os.path.join(self._ctx['BUILD_DIR'],
+                                        'php','etc','php-%s.ini' % env )
+        os.rename(new_ini_path, php_ini_path)
+
+
+# Register extension methods
+PHPConfigChooser.register(__name__)
