baceb4812e2e9b667fba7b779b08f4326e12613c
Paul Grave <paul.grave@bskyb.com>
2015-01-16 17:05:47 +0000

    fixed: _should_configure wasn’t implemented

--- extensions/phpenv/extension.py
+++ extensions/phpenv/extension.py
@@ -10,24 +10,28 @@ class PHPConfigChooser(PHPExtensionHelper):
     def __init__(self, ctx):
         PHPExtensionHelper.__init__(self, ctx)
 
+    def _should_configure(self):
+        if not self._ctx.get('ENV'):
+            return False
+
+        _log.info('Found ENV: %s' % env)
+        env = self._ctx.get('ENV')
+        self.env_specific_ini = os.path.join(self._ctx['BUILD_DIR'],
+                                      'php','etc','php-%s.ini' % env )
+
+        if os.path.exists(env_specific_ini):
+            return True
+
+        return False
+
     def _configure(self):
         """Check to see if the ENV environment variable is set. If it is
         then we look for php-${ENV}.ini and try to use that for the php.ini """
 
-        if not self._ctx.get('ENV'):
-            return
-
-        env = self._ctx.get('ENV')
-        _log.info('Found ENV: %s' % env)
         original_ini = os.path.join(self._ctx['BUILD_DIR'],
                                          'php', 'etc', 'php.ini')
-        env_specific_ini = os.path.join(self._ctx['BUILD_DIR'],
-                                        'php','etc','php-%s.ini' % env )
-        if os.path.exists(env_specific_ini):
-            os.rename(env_specific_ini, original_ini)
-            _log.info('Renamed %s to %s' % (env_specific_ini, original_ini))
-        else:
-            _log.info('%s not found' % env_specific_ini)
+        os.rename(self.env_specific_ini, original_ini)
+        _log.info('Renamed %s to %s' % (self.env_specific_ini, original_ini))
 
 # Register extension methods
 PHPConfigChooser.register(__name__)
