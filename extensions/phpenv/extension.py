9fe88b36f3fb1e8511512157ebb94f61fc9c4dc2
Paul Grave <paul.grave@bskyb.com>
2015-01-19 15:06:47 +0000

    moved: code from configure to compile stage

--- extensions/phpenv/extension.py
+++ extensions/phpenv/extension.py
@@ -10,7 +10,7 @@ class PHPConfigChooser(PHPExtensionHelper):
     def __init__(self, ctx):
         PHPExtensionHelper.__init__(self, ctx)
 
-    def _should_configure(self):
+    def _should_compile(self):
         if not self._ctx.get('ENV'):
             return False
 
@@ -25,15 +25,15 @@ class PHPConfigChooser(PHPExtensionHelper):
 
         return False
 
-    def _configure(self):
+    def _compile(self, install):
         """Check to see if the ENV environment variable is set. If it is
         then we look for php-${ENV}.ini and try to use that for the php.ini """
 
-        _log.info('Configuring')
         original_ini = os.path.join(self._ctx['BUILD_DIR'],
                                          'php', 'etc', 'php.ini')
         os.rename(self.env_specific_ini, original_ini)
         _log.info('Renamed %s to %s' % (self.env_specific_ini, original_ini))
+        install.package('PHPENV')
 
 # Register extension methods
 PHPConfigChooser.register(__name__)
